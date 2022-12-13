9c37207b15009205a988470a79339f4ee4f54e8b
Praise Disu <daviddisu8@gmail.com>
2022-12-13 14:07:46 +0100

    Run single test

--- test/BitMath.spec.ts
+++ test/BitMath.spec.ts
@@ -1,7 +1,6 @@
 import { expect } from './shared/expect'
 import { BitMathTest } from '../typechain/BitMathTest'
-import { ethers, waffle } from 'sol-test-helper'
-// import { ethers, waffle } from 'hardhat'
+import { ethers, waffle } from 'hardhat'
 import snapshotGasCost from './shared/snapshotGasCost'
 
 const { BigNumber } = ethers
