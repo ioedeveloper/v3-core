d5976409b3e3ac319614ff5a7cf19e0f9ccf7366
Praise Disu <daviddisu8@gmail.com>
2022-12-13 14:29:55 +0100

    Remove some dependencies

--- test_copy/Oracle.spec.ts
+++ test_copy/Oracle.spec.ts
@@ -1,7 +1,7 @@
 import { BigNumber, BigNumberish, Wallet } from 'ethers'
 import { ethers, waffle } from 'hardhat'
 import { OracleTest } from '../typechain/OracleTest'
-// import checkObservationEquals from './shared/checkObservationEquals'
+import checkObservationEquals from './shared/checkObservationEquals'
 // import { expect } from './shared/expect'
 // import { TEST_POOL_START_TIME } from './shared/fixtures'
 // import snapshotGasCost from './shared/snapshotGasCost'
